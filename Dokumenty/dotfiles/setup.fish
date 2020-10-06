b8a65ed6eb68126ef65ca80e93fb79153c228240
Daniel Jodłoś <danjod99@outlook.com>
2020-10-06 18:10:22 +0200

    Changed default editor

--- Dokumenty/dotfiles/setup.fish
+++ Dokumenty/dotfiles/setup.fish
@@ -3,5 +3,6 @@ omf theme sashimi
 
 set -U FZF_FIND_FILE_COMMAND 'ag -l --hidden --ignore .git . $dir 2> /dev/null'
 set -U FZF_COMPLETE '0'
+set -U EDITOR 'nvim'
 
 source $HOME/Dokumenty/dotfiles/functions.fish

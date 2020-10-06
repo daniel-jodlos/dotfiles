a98257a80dd119114562633e6169a6dc0869d83d
Daniel Jodłoś <danjod99@outlook.com>
2020-10-06 18:10:00 +0200

    Updated config for Dokumenty/dotfiles/functions.fish

--- Dokumenty/dotfiles/functions.fish
+++ Dokumenty/dotfiles/functions.fish
@@ -2,7 +2,7 @@ alias config='/usr/bin/git --git-dir=$HOME/Dokumenty/dotfiles/ --work-tree=$HOME
 
 function config_push
 	config add $argv
-	config commit -m "Updated config for $argv"
+	config commit
 	nohup timeout 30 config push -q 2>&1 &
 end
 
@@ -10,13 +10,13 @@ function edit
 	set prev (pwd)
 	cd $HOME
 	set path (config ls-tree -r master --name-only | fzf)
-	nvim $path
+	$EDITOR $path
 	config_push $path
 	cd $prev
 end
 
 function dotfile
-	nvim $argv
+	$EDITOR $argv
 	config_push $argv
 end
 

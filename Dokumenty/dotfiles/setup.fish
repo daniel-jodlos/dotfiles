f3bc84aa0bcedeb5371cdf3ba97d2b0736933e5a
Daniel Jodłoś <danjod99@outlook.com>
2020-10-06 17:35:57 +0200

    Updated config for /home/daniel/Dokumenty/dotfiles/setup.fish

--- Dokumenty/dotfiles/setup.fish
+++ Dokumenty/dotfiles/setup.fish
@@ -3,5 +3,5 @@ omf theme sashimi
 
 set -U FZF_FIND_FILE_COMMAND 'ag -l --hidden --ignore .git . $dir 2> /dev/null'
 set -U FZF_COMPLETE '0'
-alias config='/usr/bin/git --git-dir=$HOME/Dokumenty/dotfiles/ --work-tree=$HOME'
 
+source $HOME/Dokumenty/dotfiles/functions.fish

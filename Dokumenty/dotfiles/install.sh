2bc95887d74e51e404893172b8504af33d5c0b7e
Daniel Jodłoś <danjod99@outlook.com>
2020-10-06 17:30:56 +0200

    Updated config for /home/daniel/Dokumenty/dotfiles/install.sh

--- Dokumenty/dotfiles/install.sh
+++ Dokumenty/dotfiles/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syy
-sudo pacman -S git github-cli wmname fish
+sudo pacman -S git github-cli wmname fish gcc make linux-headers
 
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
@@ -18,3 +18,14 @@ gpg --keyserver hkp://keyserver.ubuntu.com --receive-keys 931FF8E79F0876134EDDBD
 curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
 yay -S spotify
 
+
+## ATTENTION! ONLY FOR TPLINK WIRELESS CARD!!!!
+# install proper driver that's not shit
+cd /tmp
+git clone https://github.com/lwfinger/rtl8192ee.git
+cd rtl8192ee
+make all
+sudo make install
+echo "blacklist rtl8192ee" | sudo tee -a /etc/modprobe.d/50-blacklist.conf
+echo "8192ee" | sudo tee -a /etc/modules-load.d/8192ee.conf
+# end of driver install

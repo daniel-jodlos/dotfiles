3eff3b5b11cd98d0dbe0c3a29773b63703255bfa
Daniel Jodłoś <danjod99@outlook.com>
2020-10-31 18:13:22 +0100

    Fixing workman script

--- Dokumenty/dotfiles/workman.bash
+++ Dokumenty/dotfiles/workman.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ $(setxkbmap -query | wc -l) -eq '4' ]; then
+if [ $(setxkbmap -query | grep variant | wc -l) -eq '0' ]; then
     setxkbmap pl -variant workman && xset r 66
 else
     setxkbmap pl && xset -r 66
